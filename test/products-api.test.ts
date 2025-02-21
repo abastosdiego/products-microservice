49d38990c7f1e8287dcfa4911d2beaaca473f391
Diego Bastos <d.bastos@marinha.mil.br>
2025-02-21 09:59:54 -0300

    testes dos endpoints de alterar e excluir

--- test/products-api.test.ts
+++ test/products-api.test.ts
@@ -17,3 +17,39 @@ test('cadastrar produto', async () => {
     expect(responseGet.status).toBe(200);
     expect(responseGet.data.descricao).toBe('Produto');
 });
+
+test('alterar produto', async () => {
+
+    const response = await axios.get('http://localhost:3000/products');
+    const id = response.data?.[0]?.['_id'];
+    const descricao = response.data?.[0]?.['descricao'];
+
+    if (!(id && descricao)){
+        throw new Error('Os dados recebidos são inválidos ou incompletos.');
+    }
+    const descricaoAlterada = `${descricao}_`;
+    const input = {
+        descricao: descricaoAlterada
+    };
+    const responseAlterar = await axios.put(`http://localhost:3000/products/${id}`, input);
+    expect(responseAlterar.status).toBe(200);
+   
+});
+
+test('excluir produto', async () => {
+
+    const responseGet = await axios.get('http://localhost:3000/products');
+    const id = responseGet.data?.[0]?.['_id'];
+    const descricao = responseGet.data?.[0]?.['descricao'];
+
+    if (!(id && descricao)){
+        throw new Error('Os dados recebidos são inválidos ou incompletos.');
+    }
+    const qtdeProdutos = responseGet.data.length;
+
+    const responseDelete = await axios.delete(`http://localhost:3000/products/${id}`);
+    expect(responseDelete.status).toBe(200);
+   
+    const responseGetAfterDelete = await axios.get('http://localhost:3000/products');
+    expect(responseGetAfterDelete.data.length).toBe(qtdeProdutos-1);
+});
