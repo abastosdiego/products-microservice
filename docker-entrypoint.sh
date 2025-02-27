9e71d2b8713e1339353001cd6df2916410d47a2e
Diego <abastosdiego@gmail.com>
2025-02-26 21:36:52 -0300

    inclusão do banco postgres

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,14 +5,18 @@ chmod -R 777 .
 
 if [ "$INSTALL_NODE_MODULES" = "true" ]; then
     yarn init -y
-    yarn add typescript ts-node nodemon express @types/express
-    yarn add dotenv # Environment variables
-    yarn add mongodb mongoose @types/mongoose # MongoDB
-    yarn add tsyringe reflect-metadata ## Dependency injection
-    yarn add --dev jest ts-jest @types/jest # Tests
-    yarn add axios # Http requests
+    yarn add typescript ts-node nodemon express @types/express dotenv mongodb mongoose @types/mongoose tsyringe reflect-metadata axios
+    yarn add --dev jest ts-jest @types/jest
+    #yarn add dotenv # Environment variables
+    #yarn add mongodb mongoose @types/mongoose # MongoDB
+    #yarn add tsyringe reflect-metadata ## Dependency injection
+    #yarn add --dev jest ts-jest @types/jest # Tests
+    #yarn add axios # Http requests
+    #yarn add sequelize sequelize-cli ## Sequelize ORM
+    #yarn add pg pg-hstore ## Postgres driver
     npx tsc --init
     yarn install --frozen-lockfile
+    #npx sequelize-cli init
 fi
 
 #npx nodemon server.js
